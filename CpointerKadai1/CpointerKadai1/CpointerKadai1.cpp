a383d4e0f4840d04d16a119e870107589da82d94
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-06-13 14:02:21 +0900

    課題１～３

--- CpointerKadai1/CpointerKadai1/CpointerKadai1.cpp
+++ CpointerKadai1/CpointerKadai1/CpointerKadai1.cpp
@@ -6,21 +6,30 @@ class MyClass final
 {
 	public: 
 	MyClass() {
-			std::cout << "生成" << std::endl; 
+        std::cout << "生成" << std::endl; 
 	}
 
-	public: 
 	~MyClass() {
 		std::cout << "破棄" << std::endl; 
 	}
-
+public:
+	std::weak_ptr<MyClass> ptr_ = {};
+	//int value = 0;
     
 };
 int main()
 {
 	{
-	std::unique_ptr<MyClass> a = std::make_unique<MyClass>();
-	
+	std::shared_ptr<MyClass> a = std::make_shared<MyClass>();
+	std::shared_ptr<MyClass> b = std::make_shared<MyClass>();
+	 
+	  a->ptr_ = b;
+	  b->ptr_ = a;
+
+	 // std::shared_ptr<MyClass> temp = a->ptr_.lock();
+
+	 // temp->value = 1;
+	 
 	}
 	return 0;
 }
