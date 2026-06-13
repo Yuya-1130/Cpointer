2259da7e7eb2703f77f4939eb6fba2f0a82cb923
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-06-13 14:53:39 +0900

    kadai3完成

---
+++ kadai4/kadai4/kadai4.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+
+void func(int) {
+	std::cout << "intが呼ばれました" << std::endl;
+	
+}
+void func(char*a) {
+		std::cout << "char*が呼ばれました" << std::endl;
+		if(a==nullptr){
+			std::cout << "引数はnullptrです" << std::endl;
+		}
+}
+int main()
+{
+	//プログラムの広義では0とNULLは別物だが、C++ではNULLは0に定義されている
+	//そのためfunc(NULL)を呼ぶとfunc(int)が呼ばれる
+	func(nullptr);
+		return 0;
+}
+
