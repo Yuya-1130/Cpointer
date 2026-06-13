1a30cc36ac7b564af0bc509b6d9883539b0f8f90
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-06-13 12:31:15 +0900

    課題１完成

---
+++ CpointerKadai1/CpointerKadai1/CpointerKadai1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <memory>
+
+class MyClass final
+{
+	public: 
+	MyClass() {
+			std::cout << "生成" << std::endl; 
+	}
+
+	public: 
+	~MyClass() {
+		std::cout << "破棄" << std::endl; 
+	}
+
+    public:
+	std::unique_ptr<MyClass> ptr_ = {};
+	int value_ = 0;
+
+};
+int main()
+{
+	{
+	std::unique_ptr<MyClass> a = std::make_unique<MyClass>();
+	
+	}
+	return 0;
+}
+
